a56e15f1ee7838589d7aa6ef7104d57b81c8778e
Adi <undip.nugroho@gmail.com>
2024-06-28 13:46:59 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,40 +1,23 @@
-import altair as alt
-import numpy as np
-import pandas as pd
-import streamlit as st
+import streamlit as st 
+#import pandas as pd 
+#import requests
+#from st_aggrid import AgGrid
 
-"""
-# Welcome to Streamlit!
+#house = pd.read_csv('house_clean.csv')
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:.
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+#def main() : 
+  #st.write('Minimal Example')
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+#if __name__ == '_main_' : 
+  #main()
 
-num_points = st.slider("Number of points in spiral", 1, 10000, 1100)
-num_turns = st.slider("Number of turns in spiral", 1, 300, 31)
+def main() : 
+  st.header('Halaman Streamlit Undip')
+  st.subheader('This is SubHeader')
+  st.markdown('# Rendering Markdown ')
+  st.write('Some Phytagorean Equation : ')
+  st.latex('c^2 = a^2+b^2')
 
-indices = np.linspace(0, 1, num_points)
-theta = 2 * np.pi * num_turns * indices
-radius = indices
-
-x = radius * np.cos(theta)
-y = radius * np.sin(theta)
-
-df = pd.DataFrame({
-    "x": x,
-    "y": y,
-    "idx": indices,
-    "rand": np.random.randn(num_points),
-})
-
-st.altair_chart(alt.Chart(df, height=700, width=700)
-    .mark_point(filled=True)
-    .encode(
-        x=alt.X("x", axis=None),
-        y=alt.Y("y", axis=None),
-        color=alt.Color("idx", legend=None, scale=alt.Scale()),
-        size=alt.Size("rand", legend=None, scale=alt.Scale(range=[1, 150])),
-    ))
+  #st.dataframe(house)
+if __name__ == '__main__' :
+   main()
