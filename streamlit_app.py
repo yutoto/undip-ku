e2046bf4bda6007733e2c0aa4cb2248d51bab0b3
Adi <undip.nugroho@gmail.com>
2024-06-28 14:12:01 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,7 +13,8 @@ if __name__ == '__main__' :
 
 def main() :
   #untuk olah text
-  st.header('Halaman Streamlit milik Undip. Masih belajar.')
+  st.header('Halaman Streamlit milik Undip.') 
+  st.header('Masih belajar.')
   st.subheader('This is SubHeader')
   st.markdown('# Rendering Markdown ')
   st.write('Some Phytagorean Equation : ')
